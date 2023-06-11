340d5efd0660bc8881a856544721691ef927584f
syarif syarifuddin <65828433+soden46@users.noreply.github.com>
2023-06-11 20:22:12 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,280 +1,125 @@
-var syarifcanvas = document.getElementById("syarifcanvas");
+var b = document.body,
+    c = document.createElement('canvas');
 
-syarifcanvas.width = window.innerWidth;
-syarifcanvas.height = window.innerHeight;
+b.appendChild(c);
 
-// Initialize the GL context
-var gl = syarifcanvas.getContext('webgl');
-if(!gl){
-  console.error("Unable to initialize WebGL.");
+var a = c.getContext('2d'),
+    e = [],
+    h = [],
+    O = c.width = innerWidth,
+    Q = c.height = innerHeight,
+    v = 32,
+    M = Math,
+    R = M.random,
+    F = M.floor,
+    C = M.cos,
+    Y = 6.3,
+    i = 0, H, S, B, f, k;
+
+for(i; i < Y; i += .2) {
+    h.push([
+        O / 2 + 180 * M.pow(M.sin(i), 3),
+        Q / 2 + 10 * (-(15 * C(i) - 5 * C(2 * i) - 2 * C(3 * i) - C(4 * i)))
+    ])
 }
 
-//Time
-var time = 0.0;
+i = 0;
 
-//************** Shader sources **************
+while(i < v) {
 
-var vertexSource = `
-attribute vec2 position;
-void main() {
-    gl_Position = vec4(position, 0.0, 1.0);
-}
-`;
+    x = R() * O;
+    y = R() * Q;
+    H = 0;
+    S = R() * 40 + 60;
+    B = R() * 60 + 20;
+    f = [];
+    k = 0;
 
-var fragmentSource = `
-precision highp float;
-
-uniform float width;
-uniform float height;
-vec2 resolution = vec2(width, height);
-
-uniform float time;
-
-#define POINT_COUNT 8
-
-vec2 points[POINT_COUNT];
-const float speed = -0.5;
-const float len = 0.25;
-float intensity = 1.3;
-float radius = 0.008;
-
-//https://www.shadertoy.com/view/MlKcDD
-//Signed distance to a quadratic bezier
-float sdBezier(vec2 pos, vec2 A, vec2 B, vec2 C){    
-    vec2 a = B - A;
-    vec2 b = A - 2.0*B + C;
-    vec2 c = a * 2.0;
-    vec2 d = A - pos;
-
-    float kk = 1.0 / dot(b,b);
-    float kx = kk * dot(a,b);
-    float ky = kk * (2.0*dot(a,a)+dot(d,b)) / 3.0;
-    float kz = kk * dot(d,a);      
-
-    float res = 0.0;
-
-    float p = ky - kx*kx;
-    float p3 = p*p*p;
-    float q = kx*(2.0*kx*kx - 3.0*ky) + kz;
-    float h = q*q + 4.0*p3;
-
-    if(h >= 0.0){ 
-        h = sqrt(h);
-        vec2 x = (vec2(h, -h) - q) / 2.0;
-        vec2 uv = sign(x)*pow(abs(x), vec2(1.0/3.0));
-        float t = uv.x + uv.y - kx;
-        t = clamp( t, 0.0, 1.0 );
-
-        // 1 root
-        vec2 qos = d + (c + b*t)*t;
-        res = length(qos);
-    }else{
-        float z = sqrt(-p);
-        float v = acos( q/(p*z*2.0) ) / 3.0;
-        float m = cos(v);
-        float n = sin(v)*1.732050808;
-        vec3 t = vec3(m + m, -n - m, n - m) * z - kx;
-        t = clamp( t, 0.0, 1.0 );
-
-        // 3 roots
-        vec2 qos = d + (c + b*t.x)*t.x;
-        float dis = dot(qos,qos);
-        
-        res = dis;
-
-        qos = d + (c + b*t.y)*t.y;
-        dis = dot(qos,qos);
-        res = min(res,dis);
-        
-        qos = d + (c + b*t.z)*t.z;
-        dis = dot(qos,qos);
-        res = min(res,dis);
-
-        res = sqrt( res );
+    while(k < v) {
+        f[k++] = {
+            x: x,
+            y: y,
+            X: 0,
+            Y: 0,
+            R: (1 - k / v) + 1,
+            S: R() + 1,
+            q: F(R() * v),
+            D: i % 2 * 2 - 1,
+            F: R() * .2 + .7,
+            f: "hsla(" + F(H) + "," + F(S) + "%," + F(B) + "%,.3)"
+        }
     }
-    
-    return res;
+
+    e[i++] = f;
 }
 
-
-//http://mathworld.wolfram.com/HeartCurve.html
-vec2 getHeartPosition(float t){
-    return vec2(16.0 * sin(t) * sin(t) * sin(t),
-                            -(13.0 * cos(t) - 5.0 * cos(2.0*t)
-                            - 2.0 * cos(3.0*t) - cos(4.0*t)));
+function render(p) {
+    a.fillStyle = p.f;
+    a.beginPath();
+    a.arc(p.x, p.y, p.R, 0, Y, 1);
+    a.closePath();
+    a.fill();
 }
 
-//https://www.shadertoy.com/view/3s3GDn
-float getGlow(float dist, float radius, float intensity){
-    return pow(radius/dist, intensity);
-}
+function loop() {
 
-float getSegment(float t, vec2 pos, float offset, float scale){
-    for(int i = 0; i < POINT_COUNT; i++){
-        points[i] = getHeartPosition(offset + float(i)*len + fract(speed * t) * 6.28);
+    a.fillStyle = "rgba(0,0,0,.2)";
+    a.fillRect(0, 0, O, Q);
+
+    var i = v, f, u, q, D, E, G;
+
+    while(i--) {
+        f = e[i];
+        u = f[0];
+        q = h[u.q];
+        D = u.x - q[0];
+        E = u.y - q[1];
+        G = M.sqrt((D * D) + (E * E));
+
+        if(G < 10) {
+            if(R() > .95) {
+                u.q = F(R() * v);
+            } else {
+                if(R() > .99) u.D *= -1;
+                u.q += u.D;
+                u.q %= v;
+                if(u.q < 0) u.q += v;
+            }
+        }
+
+        u.X += -D / G * u.S;
+        u.Y += -E / G * u.S;
+
+        u.x += u.X;
+        u.y += u.Y;
+
+        render(u);
+
+        u.X *= u.F;
+        u.Y *= u.F;
+
+        var k = 0, T, N;
+
+        while(k < v - 1) {
+            T = f[k];
+            N = f[++k];
+
+            N.x -= (N.x - T.x) * .7;
+            N.y -= (N.y - T.y) * .7;
+
+            render(N);
+        }
     }
-    
-    vec2 c = (points[0] + points[1]) / 2.0;
-    vec2 c_prev;
-    float dist = 10000.0;
-    
-    for(int i = 0; i < POINT_COUNT-1; i++){
-        //https://tinyurl.com/y2htbwkm
-        c_prev = c;
-        c = (points[i] + points[i+1]) / 2.0;
-        dist = min(dist, sdBezier(pos, scale * c_prev, scale * points[i], scale * c));
-    }
-    return max(0.0, dist);
 }
 
-void main(){
-    vec2 uv = gl_FragCoord.xy/resolution.xy;
-    float widthHeightRatio = resolution.x/resolution.y;
-    vec2 centre = vec2(0.5, 0.5);
-    vec2 pos = centre - uv;
-    pos.y /= widthHeightRatio;
-    //Shift upwards to centre heart
-    pos.y += 0.02;
-    float scale = 0.000015 * height;
-    
-    float t = time;
-    
-    //Get first segment
-  float dist = getSegment(t, pos, 0.0, scale);
-  float glow = getGlow(dist, radius, intensity);
-  
-  vec3 col = vec3(0.0);
+(function doit() {
+    window.requestAnimationFrame =
+        window.requestAnimationFrame ||
+        window.webkitRequestAnimationFrame ||
+        window.mozRequestAnimationFrame ||
+        window.oRequestAnimationFrame ||
+        window.msRequestAnimationFrame;
 
-    //White core
-  col += 10.0*vec3(smoothstep(0.003, 0.001, dist));
-  //Pink glow
-  col += glow * vec3(1.0,0.05,0.3);
-  
-  //Get second segment
-  dist = getSegment(t, pos, 3.4, scale);
-  glow = getGlow(dist, radius, intensity);
-  
-  //White core
-  col += 10.0*vec3(smoothstep(0.003, 0.001, dist));
-  //Blue glow
-  col += glow * vec3(0.1,0.4,1.0);
-        
-    //Tone mapping
-    col = 1.0 - exp(-col);
-
-    //Gamma
-    col = pow(col, vec3(0.4545));
-
-    //Output to screen
-     gl_FragColor = vec4(col,1.0);
-}
-`;
-
-//************** Utility functions **************
-
-window.addEventListener('resize', onWindowResize, false);
-
-function onWindowResize(){
-  syarifcanvas.width  = window.innerWidth;
-  syarifcanvas.height = window.innerHeight;
-    gl.viewport(0, 0, syarifcanvas.width, syarifcanvas.height);
-  gl.uniform1f(widthHandle, window.innerWidth);
-  gl.uniform1f(heightHandle, window.innerHeight);
-}
-
-
-//Compile shader and combine with source
-function compileShader(shaderSource, shaderType){
-  var shader = gl.createShader(shaderType);
-  gl.shaderSource(shader, shaderSource);
-  gl.compileShader(shader);
-  if(!gl.getShaderParameter(shader, gl.COMPILE_STATUS)){
-      throw "Shader compile failed with: " + gl.getShaderInfoLog(shader);
-  }
-  return shader;
-}
-
-//From https://codepen.io/jlfwong/pen/GqmroZ
-//Utility to complain loudly if we fail to find the attribute/uniform
-function getAttribLocation(program, name) {
-  var attributeLocation = gl.getAttribLocation(program, name);
-  if (attributeLocation === -1) {
-      throw 'Cannot find attribute ' + name + '.';
-  }
-  return attributeLocation;
-}
-
-function getUniformLocation(program, name) {
-  var attributeLocation = gl.getUniformLocation(program, name);
-  if (attributeLocation === -1) {
-      throw 'Cannot find uniform ' + name + '.';
-  }
-  return attributeLocation;
-}
-
-//************** Create shaders **************
-
-//Create vertex and fragment shaders
-var vertexShader = compileShader(vertexSource, gl.VERTEX_SHADER);
-var fragmentShader = compileShader(fragmentSource, gl.FRAGMENT_SHADER);
-
-//Create shader programs
-var program = gl.createProgram();
-gl.attachShader(program, vertexShader);
-gl.attachShader(program, fragmentShader);
-gl.linkProgram(program);
-
-gl.useProgram(program);
-
-//Set up rectangle covering entire canvas 
-var vertexData = new Float32Array([
-  -1.0,  1.0,     // top left
-  -1.0, -1.0,     // bottom left
-   1.0,  1.0,     // top right
-   1.0, -1.0,     // bottom right
-]);
-
-//Create vertex buffer
-var vertexDataBuffer = gl.createBuffer();
-gl.bindBuffer(gl.ARRAY_BUFFER, vertexDataBuffer);
-gl.bufferData(gl.ARRAY_BUFFER, vertexData, gl.STATIC_DRAW);
-
-// Layout of our data in the vertex buffer
-var positionHandle = getAttribLocation(program, 'position');
-
-gl.enableVertexAttribArray(positionHandle);
-gl.vertexAttribPointer(positionHandle,
-  2,                 // position is a vec2 (2 values per component)
-  gl.FLOAT, // each component is a float
-  false,         // don't normalize values
-  2 * 4,         // two 4 byte float components per vertex (32 bit float is 4 bytes)
-  0                 // how many bytes inside the buffer to start from
-  );
-
-//Set uniform handle
-var timeHandle = getUniformLocation(program, 'time');
-var widthHandle = getUniformLocation(program, 'width');
-var heightHandle = getUniformLocation(program, 'height');
-
-gl.uniform1f(widthHandle, window.innerWidth);
-gl.uniform1f(heightHandle, window.innerHeight);
-
-var lastFrame = Date.now();
-var thisFrame;
-
-function draw(){
-    
-  //Update time
-    thisFrame = Date.now();
-  time += (thisFrame - lastFrame)/1000;    
-    lastFrame = thisFrame;
-
-    //Send uniforms to program
-  gl.uniform1f(timeHandle, time);
-  //Draw a triangle strip connecting vertices 0-4
-  gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
-
-  requestAnimationFrame(draw);
-}
-
-draw();
+    window.requestAnimationFrame(doit);
+    loop();
+})();
