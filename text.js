9124dfe4cb222e48d97a5403f4525d5749353ed2
syarif syarifuddin <65828433+soden46@users.noreply.github.com>
2023-06-11 19:45:13 +0700

    Create text.js

---
+++ text.js
@@ -0,0 +1,7 @@
+let textcanvas = document.getElementById('textcanvas');
+let ctx = textcanvas.getContext('2d');
+
+ctx.font = 'bold 88px Arial';
+ctx.strokeStyle = 'blue';
+ctx.lineWidth = 5;
+ctx.strokeText("For My Love, Aku mungkin tidak selalu bersamamu Tapi saat kita berjauhan Ingatlah kamu akan bersamaku Tepat dalam hatiku", 10, 50);
